3eebc8b9e9db120525299ade940ac88fd3eb2f21
guaimeng1 <15034268711@163.com>
2018-12-14 20:06:20 +0800

    第一次提交到Git服务器

---
+++ src/main/java/com/tt/zhangshuai1/Zhangshuai1Application.java
@@ -0,0 +1,14 @@
+package com.tt.zhangshuai1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zhangshuai1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zhangshuai1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tt/zhangshuai1/Zhangshuai1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tt.zhangshuai1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Zhangshuai1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
